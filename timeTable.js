b6f623cc6236ea62e4916874c6c20f9826c3def4
CompileArtisan <praanesh.b.nair@gmail.com>
2024-05-08 21:22:54 +0530

    Update logo and navigation labels

--- timeTable.js
+++ timeTable.js
@@ -1,133 +1,189 @@
-const Sunday =[
-    {   
-        time: 'Sunday',
-        roomNumber: 'Holiday',
-        subject: 'No class Available',
-        type: ''
-    }
-]
-const Monday =[
-    {   
-        time: '09-10 AM',
-        roomNumber: '38-718',
-        subject: 'DBMS130',
-        type: 'Lecture'
-    },
-    {   
-        time: '10-11 AM',
-        roomNumber: '38-718',
-        subject: 'MTH166',
-        type: 'Tutorial'
-    },
-    {   
-        time: '12-01 PM',
-        roomNumber: '38-718',
-        subject: 'NS200',
-        type: 'Lecture'
-    }
-]
-const Tuesday =[
-    {   
-        time: '09-10 AM',
-        roomNumber: '27-304Y',
-        subject: 'MTH166',
-        type: 'Tutorial'
-    },
-    {   
-        time: '11-12 AM',
-        roomNumber: '28-107',
-        subject: 'CS849',
-        type: 'Lecture'
-    },
-    {   
-        time: '12-01 PM',
-        roomNumber: '28-107',
-        subject: 'CS849',
-        type: 'Lecture'
-    },
-    {   
-        time: '02-03 PM',
-        roomNumber: '38-718',
-        subject: 'NS200',
-        type: 'Lecture'
-    }
-]
+// const Sunday =[
+//     {   
+//         time: 'Sunday',
+//         roomNumber: 'Holiday',
+//         subject: 'No class Available',
+//         type: ''
+//     }
+// ]
+// const Monday =[
+//     {   
+//         time: '09-10 AM',
+//         roomNumber: '38-718',
+//         subject: 'DBMS130',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '10-11 AM',
+//         roomNumber: '38-718',
+//         subject: 'MTH166',
+//         type: 'Tutorial'
+//     },
+//     {   
+//         time: '12-01 PM',
+//         roomNumber: '38-718',
+//         subject: 'NS200',
+//         type: 'Lecture'
+//     }
+// ]
+// const Tuesday =[
+//     {   
+//         time: '09-10 AM',
+//         roomNumber: '27-304Y',
+//         subject: 'MTH166',
+//         type: 'Tutorial'
+//     },
+//     {   
+//         time: '11-12 AM',
+//         roomNumber: '28-107',
+//         subject: 'CS849',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '12-01 PM',
+//         roomNumber: '28-107',
+//         subject: 'CS849',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '02-03 PM',
+//         roomNumber: '38-718',
+//         subject: 'NS200',
+//         type: 'Lecture'
+//     }
+// ]
 
-const Wednesday =[
-    {   
-        time: '10-11 AM',
-        roomNumber: '33-309',
-        subject: 'DBMS130',
-        type: 'Lecture'
-    },
-    {   
-        time: '11-12 AM',
-        roomNumber: '38-719',
-        subject: 'CS200',
-        type: 'Lecture'
-    }
-]
+// const Wednesday =[
+//     {   
+//         time: '10-11 AM',
+//         roomNumber: '33-309',
+//         subject: 'DBMS130',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '11-12 AM',
+//         roomNumber: '38-719',
+//         subject: 'CS200',
+//         type: 'Lecture'
+//     }
+// ]
 
-const Thursday =[
-    {   
-        time: '11-12 AM',
-        roomNumber: '33-309',
-        subject: 'MTH166',
-        type: 'Lecture'
-    },
-    {   
-        time: '01-02 PM',
-        roomNumber: '38-719',
-        subject: 'CS849',
-        type: 'Lecture'
-    },
-    {   
-        time: '02-03 PM',
-        roomNumber: '38-718',
-        subject: 'NS200',
-        type: 'Lecture'
-    }
-]
+// const Thursday =[
+//     {   
+//         time: '11-12 AM',
+//         roomNumber: '33-309',
+//         subject: 'MTH166',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '01-02 PM',
+//         roomNumber: '38-719',
+//         subject: 'CS849',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '02-03 PM',
+//         roomNumber: '38-718',
+//         subject: 'NS200',
+//         type: 'Lecture'
+//     }
+// ]
 
-const Friday =[
-    {   
-        time: '10-11 AM',
-        roomNumber: '33-309',
-        subject: 'MEC103',
-        type: 'Lecture'
-    },
-    {   
-        time: '11-12 AM',
-        roomNumber: '33-309',
-        subject: 'MEC103',
-        type: 'Lecture'
-    },
-    {   
-        time: '02-03 PM',
-        roomNumber: '33-601',
-        subject: 'CS849',
-        type: 'Tutorial'
-    },
+// const Friday =[
+//     {   
+//         time: '10-11 AM',
+//         roomNumber: '33-309',
+//         subject: 'MEC103',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '11-12 AM',
+//         roomNumber: '33-309',
+//         subject: 'MEC103',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '02-03 PM',
+//         roomNumber: '33-601',
+//         subject: 'CS849',
+//         type: 'Tutorial'
+//     },
 
-]
+// ]
 
-const Saturday =[
-    {   
-        time: '09-10 AM',
-        roomNumber: '34-604',
-        subject: 'DBMS130',
-        type: 'Tutorial'
+// const Saturday =[
+//     {   
+//         time: '09-10 AM',
+//         roomNumber: '34-604',
+//         subject: 'DBMS130',
+//         type: 'Tutorial'
+//     },
+//     {   
+//         time: '10-11 AM',
+//         roomNumber: '34-604',
+//         subject: 'DBMS130',
+//         type: 'Lecture'
+//     },
+//     {   
+//         time: '01-02 PM',
+//         roomNumber: '33-309',
+//         subject: 'MTH166',
+//         type: 'Lecture'
+//     }
+// ]
+
+
+const waterStats = [
+  {
+    place: "India",
+    stats: {
+      averageRainfall: "3,700 mm/year (Highly variable)",
+      waterQuality: "60 (Moderate)",
+      fluorideLevel: "0.5 - 1.5 ppm (Depending on region)",
+      percentageTreatedWater: "81% (Urban areas)", // Source: https://www.unicef.org/india/topics/water-sanitation-and-hygiene
     },
-    {   
-        time: '10-11 AM',
-        roomNumber: '34-604',
-        subject: 'DBMS130',
-        type: 'Lecture'
+  },
+  {
+    place: "California, USA",
+    stats: {
+      averageRainfall: "34 inches/year (Highly variable)",
+      waterQuality: "80 (Very Good)",
+      waterHardness: "Varies by location (Generally Moderately Hard)",
+      agriculturalWaterUse: "80% of total water use", // Source: https://cwc.ca.gov/-/media/CWC-Website/Files/Documents/2019/06_June/June2019_Item_12_Attach_2_PPICFactSheets.pdf
     },
-    {   
-        time: '01-02 PM',
-        roomNumber: '33-309',
-        subject: 'MTH166',
-        type: 'Lecture'
-    }
-]
+  },
+  {
+    place: "Nile River Basin (Africa)",
+    stats: {
+      averageRainfall: "28 inches/year (Varies greatly)",
+      waterQuality: "65 (Moderate)",
+      turbidity: "High during wet season (due to sediment)",
+      riverDischarge: "2,830 cubic kilometers/year", // Source: https://nilebasin.org/sites/default/files/2023-09/State%2520of%2520Basin%2520Report%25202021_0.pdf
+    },
+  },
+  {
+    place: "São Paulo, Brazil",
+    stats: {
+      averageRainfall: "1,400 mm/year", // Source: https://weatherspark.com/y/30268/Average-Weather-in-S%C3%A3o-Paulo-Brazil-Year-Round
+      waterSecurity: "Moderate (Risk of future shortages)",  // Source: https://www.wri.org/data/water-stress-country
+      waterTreatment: "Mostly treated with chlorine and flocculation", // Source: https://www.scielo.br/j/ambiagua/a/SvJLKwxvwbKRCLNpKZt86Ls/?lang=en
+    },
+  },
+  {
+    place: "England, UK",
+    stats: {
+      averageRainfall: "900 mm/year", // Source: https://www.metoffice.gov.uk/research/climate/maps-and-data/uk-climate-averages
+      waterStress: "Low",
+      waterMetering: "Nearly all households have water meters", // Source: https://www.water.org.uk/
+    },
+  },
+  {
+    place: "Beijing, China",
+    stats: {
+      averageRainfall: "500 mm/year", // Source: https://weatherspark.com/y/131055/Average-Weather-in-Beijing-China-Year-Round
+      waterPollution: "Moderate to high (due to industrial activity)", // Source: https://www.worldwildlife.org/blog-posts/building-freshwater-resilience-in-china-and-beyond
+      desalinationPlants: "Increasing use for coastal water supply", // Source: https://thediplomat.com/countries/china/
+    },
+  },
+];
+
