9bfc966b6a620335ce7c1f60d271970a0e72995c
CompileArtisan <praanesh.b.nair@gmail.com>
2024-05-21 22:04:19 +0530

    Update gallery with improved marquee behavior and container

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 document.getElementById('topsy').addEventListener('mouseover', function() {
-    this.style.transform = 'scale(0.9)';
+    this.style.transform = 'scale(0.97)';
     document.getElementById('extra').style.display = 'flex';
-    document.getElementById('extra').style.transform = 'scale(1.1) translateX(-300px) translateY(-300px)';
+    document.getElementById('extra').style.transform = 'translateX(-250px) translateY(-300px)';
     document.getElementById('extra').style.position = 'relative';
     document.getElementById('extra').style.zIndex = '1';
+    setTimeout(function() {
+        document.getElementById('extra').style.transform = 'scale(1.1) translateX(-300px) translateY(-300px)';
+    }, 25);
 });
 
 document.getElementById('topsy').addEventListener('mouseout', function() {
